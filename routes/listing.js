9c7722e3c8555ababb77e9f4d369b0d09b5d2876
Khalid Agha <s.khalidagha2000@gmail.com>
2025-10-17 19:37:09 +0500

    Set up Cloudinary for image uploads and configured project dependencies

--- routes/listing.js
+++ routes/listing.js
@@ -4,15 +4,21 @@ const wrapAsync = require("../public/utils/wrapAsync.js");
 const Listing = require("../Models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js")
+const upload = multer({storage});
 
 // /listings routes combined
 router.route("/")
     .get(wrapAsync(listingController.index))
-    .post(
-        isLoggedIn,
-        validateListing,
-        wrapAsync(listingController.createListing)
-    );
+    // .post(
+    //     isLoggedIn,
+    //     validateListing,
+    //     wrapAsync(listingController.createListing)
+    // );
+    .post(upload.single('listing[image]'), (req, res) => {
+        res.send(req.file);
+    });
 
 // create listing form
 router.get("/new", isLoggedIn, listingController.renderNewForm);
