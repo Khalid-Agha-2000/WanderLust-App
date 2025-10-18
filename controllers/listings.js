c10cdb85d9b5d12fbb977ed2d20d5184ae0756b1
Khalid Agha <s.khalidagha2000@gmail.com>
2025-10-19 02:12:34 +0500

    Add Mapbox map with markers for listings, fix coordinate display and map rendering

--- controllers/listings.js
+++ controllers/listings.js
@@ -48,8 +48,8 @@ module.exports.createListing = async(req, res, next) => {
         newListing.owner = req.user._id;
         newListing.image = {url, filename};
     }
-    newListing.geometry = response.body.features[0].geometry;
 
+    newListing.geometry = response.body.features[0].geometry;
     let savedListing = await newListing.save();
     console.log(savedListing);
     req.flash("success", "New Listing Created!");
--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,11 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    center: [28.9784, 41.0082],
-    zoom: 9
+    style: "mapbox://styles/mapbox/outdoors-v12",
+    center: coordinates,
+    zoom: 10,
 });
 
-console.log(coordinates);
-
-// const marker = new mapboxgl.Marker()
-//     .setLngLat([12.554729, 5570651])
-//     .addTo(map);
+const marker = new mapboxgl.Marker({ color: "red"})
+    .setLngLat(coordinates)
+    .addTo(map);
